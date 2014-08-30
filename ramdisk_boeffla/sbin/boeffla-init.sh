19e8d9f2c2056c58ac4c74be00bfa80afed98993
ZaneZam <cyxman@yahoo.com>
2014-08-30 19:51:35 +0200

    Boeffla ramfs: Put additional sync lines in for improved consistency
    (there were issues with installing the busybox apps)

--- ramdisk_boeffla/sbin/boeffla-init.sh
+++ ramdisk_boeffla/sbin/boeffla-init.sh
@@ -97,6 +97,7 @@ FRANDOM_ENABLER="/data/.boeffla/enable-frandom"
 		echo $(date) "Mount/umount/top applet symlinks installed to /system/xbin" >> $BOEFFLA_LOGFILE
 	
 	fi
+	/sbin/busybox sync
 	mount -o remount,ro -t ext4 $SYSTEM_DEVICE /system
 		
 # Correct /sbin and /res directory and file permissions
@@ -152,11 +153,11 @@ FRANDOM_ENABLER="/data/.boeffla/enable-frandom"
 	echo $(date) Applying Boeffla-Kernel default settings >> $BOEFFLA_LOGFILE
 
 	# Ext4 tweaks default to on
-	sync
+	/sbin/busybox sync
 	mount -o remount,commit=20,noatime $CACHE_DEVICE /cache
-	sync
+	/sbin/busybox sync
 	mount -o remount,commit=20,noatime $DATA_DEVICE /data
-	sync
+	/sbin/busybox sync
 	echo $(date) Ext4 tweaks applied >> $BOEFFLA_LOGFILE
 
 	# Sdcard buffer tweaks default to 256 kb
@@ -265,6 +266,8 @@ FRANDOM_ENABLER="/data/.boeffla/enable-frandom"
 		/sbin/busybox chmod 0644 /system/etc/.installed_su_daemon
 
 		/system/bin/sh /system/etc/install-recovery.sh
+
+		/sbin/busybox sync
 
 		mount -o remount,ro -t ext4 $SYSTEM_DEVICE /system
 		echo $(date) Auto root: su installed >> $BOEFFLA_LOGFILE
