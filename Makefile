rpdroky/boeffla-kernel-cm-s3

toolchain: new directory for toolchains after build envoirment changes

Makefile
@@ -193,7 +193,7 @@ SUBARCH := $(shell uname -m | sed -e s/i.86/i386/ -e s/sun4u/sparc64/ \
 # Note: Some architectures assign CROSS_COMPILE in their arch/*/Makefile
 export KBUILD_BUILDHOST := $(SUBARCH)
 ARCH		?= arm
-CROSS_COMPILE	?= /home/zanezam/Werkstatt/entwicklung/toolchains/l493/bin/arm-eabi-
+CROSS_COMPILE	?= /werkstatt/entwicklung/toolchains/l493/bin/arm-eabi-
 
 # Architecture as present in compile.h
 UTS_MACHINE 	:= $(ARCH)

zanezam/boeffla-kernel-samsung-n51x0

Makefile
@@ -193,7 +193,7 @@ SUBARCH := $(shell uname -m | sed -e s/i.86/i386/ -e s/sun4u/sparc64/ \
 # Note: Some architectures assign CROSS_COMPILE in their arch/*/Makefile
 export KBUILD_BUILDHOST := $(SUBARCH)
 ARCH		?= arm
-CROSS_COMPILE	?= /home/zanezam/Werkstatt/entwicklung/toolchains/l493/bin/arm-eabi-
+CROSS_COMPILE	?= /werkstatt/entwicklung/toolchains/l493_a9/bin/arm-eabi-
 
 # Architecture as present in compile.h
 UTS_MACHINE 	:= $(ARCH)